9615553e57fb2fd2ad3dce2cbb4a17c881775ae7
emanuelgarnica-commits <emanuel.garnica@escola.pr.gov.br>
2026-06-10 12:36:04 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+//O JavaScript é uma linguagem WEB
+
+//Variáveis - Valores armazenados na memória do computador
+
+//Funções - Blocos de código
+
+function carregar(){
+    //alert("Bem vindo ao JavaScript")
+    var msg = window.document.getElementById('msg')
+    var img = window.document.getElementById('imagem')
+    
+    var data = new data ()
+    var hora = data.gethours()
+    var minutos = data.getminutos()
+    
+    msg.innerHTML = `agora sao ${hora}:${minutos}am`
+    }
